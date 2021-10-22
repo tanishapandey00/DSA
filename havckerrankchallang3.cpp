397f6bcaef2e125e583f5816d82fca1864be4e29
tanishapandey00 <tanishapandey00@gamil.com>
2021-10-22 12:57:21 +0530

    commiting

--- havckerrankchallang3.cpp
+++ havckerrankchallang3.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    int a=99,b=9;
-    long long c=sqrt(a);
-    cout<<c<<" "<<sqrt(a)<<" "<<c-sqrt(a)<<endl;
-    return 0;
+        int a;
+        cin>>a;
+        int arr[a];
+        for(int i=0;i<a;i++){
+                cin>>arr[i];
+        }
+         int n = sizeof(arr) / sizeof(arr[0]);
+        sort(arr, arr+n);
+        reverse(arr,arr+n);
+        while(n>0){
+                int k=arr[n-1];
+                for(int i=n-1;i>=0;i--){
+                        arr[i]-=k;
+                }
+                cout<<n<<endl;
+                while(n>0&& !arr[n-1]) n--;
+        }
+        return 0;
 }
