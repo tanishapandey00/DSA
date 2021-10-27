266ed9369a654dcca89426a5ababdd822cd68a34
tanishapandey00 <tanishapandey00@gamil.com>
2021-10-27 21:27:22 +0530

    commiting

--- geeksforgeeks7.cpp
+++ geeksforgeeks7.cpp
@@ -6,28 +6,44 @@ using namespace std;
 // I/P : arr[] ={3,4,8,-9,20,6}
 //  O/P : Yes
 // Equlibrium point is a point such that sum of element before it is equal to sum of element after it;
-void solution(int arr[],int n,int k){
-vector<int> res;
-    res[0] = arr[0];
-    for (int i = 1; i < n; i++)
+
+// Method 1
+bool solution(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        res[i] = res[i - 1] + arr[i];
+        sum += arr[i];
     }
-   if(res[k-1]==res[n-1]-res[k]){
-       cout<<"yes";
-   }
-   else{
-       cout<<"No";
-   }
+    int l_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (l_sum == sum - arr[i])
+        {
+            return true;
+        }
+        l_sum += arr[i];
+        sum -= arr[i];
+    }
+    return false;
 }
 int main()
 {
-    int n,k;
-    cin >> n>>k;
+    int n;
+    cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+    }
+    // solution(arr, n);
+    if (solution(arr, n))
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
     }
     return 0;
 }
---
+++ geeksforgeeks8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+//Question:-  Given n ranges, find the maximum appearing eleent in the range;
+// I/P :- l[1,2,5,15],R[5,8,7,18]
+//O/P :- 5
+int main(){
+    int n,m;
+    int a[m],b[m];
+    for(int i=0;i<m;i++) cin>>a[i];
+    for(int j=0;j<m;j++) cin>>b[j];
+    return 0;
+}
