8feecbafd3161f3d67be9df153b741a3f77eeedf
tanishapandey00 <tanishapandey00@gamil.com>
2021-10-27 12:47:51 +0530

    commiting

--- geeksforgeeks4.cpp
+++ geeksforgeeks4.cpp
@@ -1,8 +1,40 @@
-#include<iostream>
-#include<algoritm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-
-int main(){
-    
+// no negative array
+bool sum_arr(int arr[], int n, int sum)
+{
+    int res = arr[0], index = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (res < sum)
+        {
+            res += arr[i];
+        }
+        while (res > sum)
+        {
+            res -= arr[index];
+            index++;
+        }
+    }
+    return (res == sum);
+}
+int main()
+{
+    int n, sum;
+    cin >> n >> sum;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    if (sum_arr(arr, n, sum) == true)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
     return 0;
 }
